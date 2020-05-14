e42eefc3117429c5f115c0e0ccf5fd0aee2048be
MARCELO BUENO SILVA <LN00867@tlsbr.local>
2020-05-14 01:16:59 -0300

    Adiciondo imagem

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,20 +1,39 @@
 import React from 'react';
 import './style.css';
+import styled from 'styled-components';
+
+const Sprite = styled.img`
+  width: 5em;
+  height: 5em;
+`;
 
 export default class Card extends React.Component {
     state = {
         name: '',
+        url: '',
         imageURL: '',
-        pokemonIndex: ''
+        pokemonIndex: '',
+        imageLoading: true,
+        toManyRequests: false
     };
+
+    componentDidMount() {
+      const { name, url } = this.props;
+      const pokemonIndex = url.split('/')[url.split('/').length - 2];
+      const imageURL = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`;
+
+      this.setState({ name, url, imageURL, pokemonIndex });
+    }
     
     render() {
-      const { name, url } = this.props;
-      console.log(this.props);
-
         return (
             <div className="cardPokemon">
-              <h1>{name}</h1>
+              <h1>{this.state.name}</h1>
+              <Sprite  
+              onLoad={() => this.setState({ imageLoading: false })}
+              onError={() => this.setState({ toManyRequests: true })}
+              src={this.state.imageURL}
+              />
             </div>
         );
     }
