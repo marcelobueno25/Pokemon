88833df42ef9fe8b7f34a5a0598001110cc8b851
Marcelo Bueno <marcelobueno25@hotmail.com>
2020-05-14 14:23:48 -0300

    Ajuste no RES imagens

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,40 +1,32 @@
 import React from 'react';
-import './style.css';
-import styled from 'styled-components';
+import './style.scss';
+import axios from 'axios';
 
-const Sprite = styled.img`
-  width: 5em;
-  height: 5em;
-`;
 
 export default class Card extends React.Component {
-    state = {
-        name: '',
-        url: '',
-        imageURL: '',
-        pokemonIndex: '',
-        imageLoading: true,
-        toManyRequests: false
-    };
+  state = {
+    name: '',
+    url: '',
+    imageURL: '',
+    pokemonIndex: ''
+  };
 
-    componentDidMount() {
-      const { name, url } = this.props;
-      const pokemonIndex = url.split('/')[url.split('/').length - 2];
-      const imageURL = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`;
+  async componentDidMount() {
+    const { name, url } = this.props;
+    const res = await axios.get(url);
+    console.log(res.data);
+    const pokemonIndex = res.data.id;
+    const imageURL = res.data.sprites.front_default;
+    this.setState({ name, url, imageURL, pokemonIndex });
+  }
 
-      this.setState({ name, url, imageURL, pokemonIndex });
-    }
-    
-    render() {
-        return (
-            <div className="cardPokemon">
-              <h1>{this.state.name}</h1>
-              <Sprite  
-              onLoad={() => this.setState({ imageLoading: false })}
-              onError={() => this.setState({ toManyRequests: true })}
-              src={this.state.imageURL}
-              />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="cardPokemon">
+        <h1>{this.state.name}</h1>
+        <h4>{this.state.pokemonIndex}</h4>
+        <img alt="pokemon" src={this.state.imageURL} />
+      </div>
+    );
+  }
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,36 +1,38 @@
 import React from 'react';
 import Card from '../../components/Card';
 import axios from 'axios';
+import './style.scss';
 
 export default class Home extends React.Component {
-state = {
-  url: 'https://pokeapi.co/api/v2/pokemon/',
-  pokemons: null
-};
+  state = {
+    url: 'https://pokeapi.co/api/v2/pokemon?limit=20/',
+    pokemons: null
+  };
 
-async componentDidMount(){
-  const res = await axios.get(this.state.url);
-  this.setState({pokemons: res.data['results']});
-}
+  async componentDidMount() {
+    const res = await axios.get(this.state.url);
+    console.log(res);
+    this.setState({ pokemons: res.data['results'] });
+  }
 
   render() {
     return (
-      <React.Fragment>
+      <div id="home">
         {this.state.pokemons ? (
           <React.Fragment>
-          {this.state.pokemons.map(pokemon => (
-              <Card 
-              key={pokemon.name}
-              name={pokemon.name} 
-              url={pokemon.url}
-              teste={pokemon}
+            {this.state.pokemons.map(pokemon => (
+              <Card
+                key={pokemon.name}
+                name={pokemon.name}
+                url={pokemon.url}
+                teste={pokemon}
               />
-          ))}
+            ))}
           </React.Fragment>
         ) : (
-          <h3>Loading Pokemon</h3>
-        )}
-      </React.Fragment>
+            <h3>Loading Pokemon</h3>
+          )}
+      </div>
     );
   }
 }
